fbdebc91679cad57d45f0a71fbd261fa2a603802
Ghundo <marcavenzaid.gamil@gmail.com>
2018-06-08 23:36:48 +0800

    Fixed scripts

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,29 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    [SerializeField] private GameObject target;
-    [SerializeField] private float directionOffset = 2;
+    [SerializeField] private Transform target;
     private Vector3 initOffset;
-    private Vector3 playerPosition;    
 
     private void Start() {
         initOffset = transform.position - target.transform.position;
     }
 
     private void Update() {
-        Vector3 targetForward = target.transform.forward;
-        targetForward.y = 0;
-        float targetFacing = Quaternion.LookRotation(targetForward).eulerAngles.y;
-        Vector3 offset;
-        Debug.Log(targetFacing);
-        if (targetFacing < 180) {
-            offset = initOffset + Vector3.right * directionOffset * (90 - Mathf.Abs(90 - targetFacing)) / 90;            
-        } else {
-            offset = initOffset + Vector3.left * directionOffset * (270 - Mathf.Abs(270 - targetFacing)) / 270;
-        }
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, 0) + offset;
+        transform.position = target.position + initOffset;
     }
 }
--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,55 +5,58 @@
 [RequireComponent(typeof(Animator))]
 public class Character : MonoBehaviour {
 
-    [SerializeField] float m_MovingTurnSpeed = 360;
-    [SerializeField] float m_StationaryTurnSpeed = 180;
-    [SerializeField] float m_JumpPower = 6f;
-    [Range(1f, 4f)] [SerializeField] float m_GravityMultiplier = 1.5f;
-    [SerializeField] float m_RunCycleLegOffset = 0.2f; //specific to the character in sample assets, will need to be modified to work with others
-    [SerializeField] float m_MoveSpeedMultiplier = 1f;
-    [SerializeField] float m_AnimSpeedMultiplier = 1f;
-    [SerializeField] float m_GroundCheckDistance = 0.3f;
+    [SerializeField] float movingTurnSpeed = 360;
+    [SerializeField] float stationaryTurnSpeed = 180;
+    [SerializeField] float jumpPower = 6f;
+    [Range(1f, 4f)] [SerializeField] float gravityMultiplier = 1.5f;    
+    [SerializeField] float runCycleLegOffset = 0.2f; // Specific to the character.
+    [SerializeField] float moveSpeedMultiplier = 1f;
+    [SerializeField] float animSpeedMultiplier = 1f;
+    [SerializeField] float groundCheckDistance = 0.3f;
 
-    Rigidbody m_Rigidbody;
-    Animator m_Animator;
-    bool m_IsGrounded;
-    float m_OrigGroundCheckDistance;
-    const float k_Half = 0.5f;
-    float m_TurnAmount;
-    float m_ForwardAmount;
-    Vector3 m_GroundNormal;
-    float m_CapsuleHeight;
-    Vector3 m_CapsuleCenter;
-    CapsuleCollider m_Capsule;
-    bool m_Crouching;
+    private Rigidbody rb;
+    private Animator animator;
+    private bool isGrounded;
+    private float origGroundCheckDistance;
+    private const float half = 0.5f;
+    private float turnAmount;
+    private float forwardAmount;
+    private Vector3 groundNormal;
+    private CapsuleCollider capsule;
+    private float capsuleHeight;
+    private Vector3 capsuleCenter;
+    private float capsuleRadius;
+    private bool crouching;
+
+    float x = 0;
 
     void Start() {
-        m_Animator = GetComponent<Animator>();
-        m_Rigidbody = GetComponent<Rigidbody>();
-        m_Capsule = GetComponent<CapsuleCollider>();
-        m_CapsuleHeight = m_Capsule.height;
-        m_CapsuleCenter = m_Capsule.center;
-
-        m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
-        m_OrigGroundCheckDistance = m_GroundCheckDistance;
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        capsule = GetComponent<CapsuleCollider>();
+        capsuleHeight = capsule.height;
+        capsuleCenter = capsule.center;
+        capsuleRadius = capsule.radius;
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+                       //| RigidbodyConstraints.FreezePositionZ;
+        origGroundCheckDistance = groundCheckDistance;
     }
 
     public void Move(Vector3 move, bool crouch, bool jump) {
-
-        // convert the world relative moveInput vector into a local-relative
-        // turn amount and forward amount required to head in the desired
-        // direction.
-        if (move.magnitude > 1f) move.Normalize();
+        // Convert the world relative moveInput vector into a local-relative turn amount 
+        // and forward amount required to head in the desired direction.
+        if (move.magnitude > 1f) {
+            move.Normalize();
+        }
         move = transform.InverseTransformDirection(move);
         CheckGroundStatus();
-        move = Vector3.ProjectOnPlane(move, m_GroundNormal);
-        m_TurnAmount = Mathf.Atan2(move.x, move.z);
-        m_ForwardAmount = move.z;
+        move = Vector3.ProjectOnPlane(move, groundNormal);
+        turnAmount = Mathf.Atan2(move.x, move.z);
+        forwardAmount = move.z;
 
         ApplyExtraTurnRotation();
 
-        // control and velocity handling is different when grounded and airborne:
-        if (m_IsGrounded) {
+        if (isGrounded) {
             HandleGroundedMovement(crouch, jump);
         } else {
             HandleAirborneMovement();
@@ -62,124 +65,115 @@ public class Character : MonoBehaviour {
         ScaleCapsuleForCrouching(crouch);
         PreventStandingInLowHeadroom();
 
-        // send input and other state parameters to the animator
         UpdateAnimator(move);
     }
 
     void ScaleCapsuleForCrouching(bool crouch) {
-        if (m_IsGrounded && crouch) {
-            if (m_Crouching) return;
-            m_Capsule.height = m_Capsule.height / 2f;
-            m_Capsule.center = m_Capsule.center / 2f;
-            m_Crouching = true;
+        if (isGrounded && crouch) {
+            if (crouching) return;
+            capsule.height = capsule.height / 2f;
+            capsule.center = capsule.center / 2f;
+            crouching = true;
         } else {
-            Ray crouchRay = new Ray(m_Rigidbody.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
-            float crouchRayLength = m_CapsuleHeight - m_Capsule.radius * k_Half;
-            if (Physics.SphereCast(crouchRay, m_Capsule.radius * k_Half, crouchRayLength, Physics.AllLayers, QueryTriggerInteraction.Ignore)) {
-                m_Crouching = true;
+            Ray crouchRay = new Ray(rb.position + Vector3.up * capsule.radius * half, Vector3.up);
+            float crouchRayLength = capsuleHeight - capsule.radius * half;
+            if (Physics.SphereCast(crouchRay, capsule.radius * half, crouchRayLength, Physics.AllLayers, QueryTriggerInteraction.Ignore)) {
+                crouching = true;
                 return;
             }
-            m_Capsule.height = m_CapsuleHeight;
-            m_Capsule.center = m_CapsuleCenter;
-            m_Crouching = false;
+            capsule.height = capsuleHeight;
+            capsule.center = capsuleCenter;
+            crouching = false;
         }
     }
 
     void PreventStandingInLowHeadroom() {
-        // prevent standing up in crouch-only zones
-        if (!m_Crouching) {
-            Ray crouchRay = new Ray(m_Rigidbody.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
-            float crouchRayLength = m_CapsuleHeight - m_Capsule.radius * k_Half;
-            if (Physics.SphereCast(crouchRay, m_Capsule.radius * k_Half, crouchRayLength, Physics.AllLayers, QueryTriggerInteraction.Ignore)) {
-                m_Crouching = true;
+        if (!crouching) {
+            Ray crouchRay = new Ray(rb.position + Vector3.up * capsule.radius * half, Vector3.up);
+            float crouchRayLength = capsuleHeight - capsule.radius * half;
+            if (Physics.SphereCast(crouchRay, capsule.radius * half, crouchRayLength, Physics.AllLayers, QueryTriggerInteraction.Ignore)) {
+                crouching = true;
             }
         }
     }
 
     void UpdateAnimator(Vector3 move) {
-        // update the animator parameters
-        m_Animator.SetFloat("Forward", m_ForwardAmount, 0.1f, Time.deltaTime);
-        m_Animator.SetFloat("Turn", m_TurnAmount, 0.1f, Time.deltaTime);
-        m_Animator.SetBool("Crouch", m_Crouching);
-        m_Animator.SetBool("OnGround", m_IsGrounded);
-        if (!m_IsGrounded) {
-            m_Animator.SetFloat("Jump", m_Rigidbody.velocity.y);
+        animator.SetFloat("Forward", forwardAmount, 0.1f, Time.deltaTime);
+        animator.SetFloat("Turn", turnAmount, 0.1f, Time.deltaTime);
+        animator.SetBool("Crouch", crouching);
+        animator.SetBool("OnGround", isGrounded);
+        if (!isGrounded) {
+            animator.SetFloat("Jump", rb.velocity.y);
         }
 
-        // calculate which leg is behind, so as to leave that leg trailing in the jump animation
+        // Calculate which leg is behind, so as to leave that leg trailing in the jump animation
         // (This code is reliant on the specific run cycle offset in our animations,
         // and assumes one leg passes the other at the normalized clip times of 0.0 and 0.5)
-        float runCycle =
-            Mathf.Repeat(
-                m_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
-        float jumpLeg = (runCycle < k_Half ? 1 : -1) * m_ForwardAmount;
-        if (m_IsGrounded) {
-            m_Animator.SetFloat("JumpLeg", jumpLeg);
+        float runCycle =  Mathf.Repeat(animator.GetCurrentAnimatorStateInfo(0).normalizedTime + runCycleLegOffset, 1);
+        float jumpLeg = (runCycle < half ? 1 : -1) * forwardAmount;
+        if (isGrounded) {
+            animator.SetFloat("JumpLeg", jumpLeg);
         }
 
-        // the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
+        // The anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
         // which affects the movement speed because of the root motion.
-        if (m_IsGrounded && move.magnitude > 0) {
-            m_Animator.speed = m_AnimSpeedMultiplier;
+        if (isGrounded && move.magnitude > 0) {
+            animator.speed = animSpeedMultiplier;
         } else {
-            // don't use that while airborne
-            m_Animator.speed = 1;
+            animator.speed = 1;
         }
     }
 
     void HandleAirborneMovement() {
-        // apply extra gravity from multiplier:
-        Vector3 extraGravityForce = (Physics.gravity * m_GravityMultiplier) - Physics.gravity;
-        m_Rigidbody.AddForce(extraGravityForce);
+        Vector3 extraGravityForce = (Physics.gravity * gravityMultiplier) - Physics.gravity;
+        rb.AddForce(extraGravityForce);
 
-        m_GroundCheckDistance = m_Rigidbody.velocity.y < 0 ? m_OrigGroundCheckDistance : 0.01f;
+        groundCheckDistance = rb.velocity.y < 0 ? origGroundCheckDistance : 0.01f;
     }
 
     void HandleGroundedMovement(bool crouch, bool jump) {
-        // check whether conditions are right to allow a jump:
-        if (jump && !crouch && m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded")) {
-            // jump!
-            m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, m_JumpPower, m_Rigidbody.velocity.z);
-            m_IsGrounded = false;
-            m_Animator.applyRootMotion = false;
-            m_GroundCheckDistance = 0.1f;
+        if (jump && !crouch && animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded")) {
+            rb.velocity = new Vector3(rb.velocity.x, jumpPower, rb.velocity.z);
+            isGrounded = false;
+            animator.applyRootMotion = false;
+            groundCheckDistance = 0.1f;
         }
     }
 
     void ApplyExtraTurnRotation() {
-        // help the character turn faster (this is in addition to root rotation in the animation)
-        float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
-        transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
+        // Help the character turn faster (this is in addition to root rotation in the animation).
+        float turnSpeed = Mathf.Lerp(stationaryTurnSpeed, movingTurnSpeed, forwardAmount);
+        transform.Rotate(0, turnAmount * turnSpeed * Time.deltaTime, 0);
     }
 
     public void OnAnimatorMove() {
-        // we implement this function to override the default root motion.
-        // this allows us to modify the positional speed before it's applied.
-        if (m_IsGrounded && Time.deltaTime > 0) {
-            Vector3 v = (m_Animator.deltaPosition * m_MoveSpeedMultiplier) / Time.deltaTime;
-
-            // we preserve the existing y part of the current velocity.
-            v.y = m_Rigidbody.velocity.y;
-            m_Rigidbody.velocity = v;
+        // We implement this function to override the default root motion.
+        // This allows us to modify the positional speed before it's applied.
+        if (isGrounded && Time.deltaTime > 0) {
+            Vector3 v = (animator.deltaPosition * moveSpeedMultiplier) / Time.deltaTime;
+            
+            // We preserve the existing y part of the current velocity.
+            v.y = rb.velocity.y;
+            rb.velocity = v;
         }
+    }
+
+    void OnDrawGizmosSelected() {
+        // Helper to visualise the ground check ray in the scene view.
+        Gizmos.DrawSphere(transform.position + (Vector3.up * 0.1f), capsuleRadius);
     }
 
     void CheckGroundStatus() {
         RaycastHit hitInfo;
-#if UNITY_EDITOR
-        // helper to visualise the ground check ray in the scene view
-        Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * m_GroundCheckDistance));
-#endif
-        // 0.1f is a small offset to start the ray from inside the character
-        // it is also good to note that the transform position in the sample assets is at the base of the character
-        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance)) {
-            m_GroundNormal = hitInfo.normal;
-            m_IsGrounded = true;
-            m_Animator.applyRootMotion = true;
+        // 0.3f is a small offset to start the ray from inside the character
+        if (Physics.SphereCast(transform.position + (Vector3.up * 0.3f), capsuleRadius, Vector3.down, out hitInfo, groundCheckDistance)) {
+            groundNormal = hitInfo.normal;
+            isGrounded = true;
+            animator.applyRootMotion = true;
         } else {
-            m_IsGrounded = false;
-            m_GroundNormal = Vector3.up;
-            m_Animator.applyRootMotion = false;
+            isGrounded = false;
+            groundNormal = Vector3.up;
+            animator.applyRootMotion = false;
         }
     }
 }
